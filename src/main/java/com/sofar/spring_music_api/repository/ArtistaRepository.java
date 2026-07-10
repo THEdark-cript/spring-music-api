e3f1c94b3e40d107ad93719eadc0a678dd79eb4d
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2026-07-10 16:57:24 -0300

    refactor: remove as importações

--- src/main/java/com/sofar/spring_music_api/repository/ArtistaRepository.java
+++ src/main/java/com/sofar/spring_music_api/repository/ArtistaRepository.java
@@ -3,8 +3,5 @@ package com.sofar.spring_music_api.repository;
 import com.sofar.spring_music_api.domain.entity.Artista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public interface ArtistaRepository extends JpaRepository<Artista, Integer> {
 }
