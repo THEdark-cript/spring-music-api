92569b58bbacc4b4da9ef710a30a9263bae0a273
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2026-07-10 16:52:55 -0300

    fix: corrige a anotação @NotBlank para @NotNull para os parâmetros dataEvento, prazoVotacao e status do EventoRequestDTO

--- src/main/java/com/sofar/spring_music_api/domain/dto/evento/EventoRequestDTO.java
+++ src/main/java/com/sofar/spring_music_api/domain/dto/evento/EventoRequestDTO.java
@@ -2,6 +2,7 @@ package com.sofar.spring_music_api.domain.dto.evento;
 
 import com.sofar.spring_music_api.domain.enums.EventoStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
@@ -14,8 +15,8 @@ public record EventoRequestDTO(
         @NotBlank @Size(max = 150) String cidade,
         @NotBlank String logradouro,
         @NotBlank String bairro,
-        @NotBlank LocalDateTime dataEvento,
-        @NotBlank LocalDate prazoVotacao,
-        @NotBlank EventoStatus status
+        @NotNull LocalDateTime dataEvento,
+        @NotNull LocalDate prazoVotacao,
+        @NotNull EventoStatus status
         ) {
 }
