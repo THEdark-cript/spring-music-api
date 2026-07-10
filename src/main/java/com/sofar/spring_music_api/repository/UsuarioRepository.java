7525674b65465f6b57555bfb1ce744355a45fe32
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2026-07-10 16:46:44 -0300

    feat: altera o tipo do usuário retornado na busca pelo email e adiciona o dto de resposta para usuário

--- src/main/java/com/sofar/spring_music_api/repository/UsuarioRepository.java
+++ src/main/java/com/sofar/spring_music_api/repository/UsuarioRepository.java
@@ -2,8 +2,7 @@ package com.sofar.spring_music_api.repository;
 
 import com.sofar.spring_music_api.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    UserDetails findByEmail(String email);
+    Usuario findByEmail(String email);
 }
